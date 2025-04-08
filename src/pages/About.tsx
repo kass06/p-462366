5df59e7b1d5d49727bfd9d803735cbaebfa96582
choy Marame <139590125+kass06@users.noreply.github.com>
2025-04-08 18:33:58 +0200

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -99,7 +99,7 @@ const About = () => {
               <div className="w-6/12 ml-5 max-md:w-full max-md:ml-0">
                 <div className="bg-[rgba(151,161,175,1)] flex w-[793px] shrink-0 max-w-full h-[793px] mx-auto max-md:mt-10 relative overflow-hidden">
                   <img 
-                    src="/public/lovable-uploads/89407e41-ac74-4101-8c0c-d8955a9eeac2.png" 
+                    src="public/lovable-uploads/044feab2-f6bd-40c8-83a3-00f4719fbad9.png" 
                     alt="Iketle Maleka" 
                     className="absolute inset-0 w-full h-full object-cover"
                   />
